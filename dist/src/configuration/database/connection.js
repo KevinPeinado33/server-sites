dcb61e25ed4afb69186d33e575ab85cec71c917e
Alumno Facultad de Ingenieria L1 <alum.fial1@lim.upeu.edu.pe>
2022-10-27 15:20:18 -0500

    conexion real gaaa

--- dist/src/configuration/database/connection.js
+++ dist/src/configuration/database/connection.js
@@ -5,10 +5,10 @@ const user_model_1 = require("../../user/domain/model/user.model");
 const db = new sequelize_typescript_1.Sequelize({
     repositoryMode: true,
     dialect: 'postgres',
-    host: 'bahwhlmqa3asozcb1qq6-postgresql.services.clever-cloud.com',
-    username: 'ulqwrochhmlwhjl5mnmk',
-    password: 'FvmHVAAs5essB2rjkOym',
-    database: 'bahwhlmqa3asozcb1qq6',
+    host: '129.151.123.131',
+    username: 'postgres',
+    password: 'example',
+    database: 'postgres',
     // logging: false,
     models: [user_model_1.UserModel]
 });
--- src/configuration/database/connection.ts
+++ src/configuration/database/connection.ts
@@ -5,10 +5,10 @@ import { UserModel } from '../../user/domain/model/user.model'
 const db = new Sequelize({
     repositoryMode: true,
     dialect: 'postgres',
-    host: 'bahwhlmqa3asozcb1qq6-postgresql.services.clever-cloud.com',
-    username: 'ulqwrochhmlwhjl5mnmk',
-    password: 'FvmHVAAs5essB2rjkOym',
-    database: 'bahwhlmqa3asozcb1qq6',
+    host: '129.151.123.131',
+    username: 'postgres',
+    password: 'example',
+    database: 'postgres',
     // logging: false,
     models: [ UserModel ]
 })
